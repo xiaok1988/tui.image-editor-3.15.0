32ea980b1bf5b6f0f0fd349fd26fbec60fe9f642
xiaokk <chengkaikelly@126.com>
2026-05-16 14:01:13 +0800

    fix some issues

--- apps/image-editor/src/js/component/filter.js
+++ apps/image-editor/src/js/component/filter.js
@@ -141,7 +141,6 @@ class Filter extends Component {
    * @private
    */
   _apply(sourceImg, callback) {
-    sourceImg.filters.push();
     const result = sourceImg.applyFilters();
     if (result) {
       callback();
