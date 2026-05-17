fed6638e9de122cdb680fb6a2f73078a955b2aae
xiaokk <chengkaikelly@126.com>
2026-05-18 01:36:17 +0800

    adjust ai services

--- apps/angular-image-editor/api/generate-image.js
+++ apps/angular-image-editor/api/generate-image.js
@@ -9,7 +9,7 @@ const MODELS = {
 
 function hfRequest(modelId, apiToken, body) {
   return new Promise((resolve, reject) => {
-    const url = `https://router.huggingface.co/hf-inference/models/${modelId}`;
+    const url = `https://router.huggingface.co/hf-inference/models/${MODELS[modelId]}`;
     console.log('HF request URL:', url);
 
     const data = JSON.stringify(body);
